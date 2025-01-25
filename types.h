6fce0c783238ce24eec33ce14d0f3fdda1abbfcc
N.Yaswanth <yaswanth.n@priyarajaelectronics.com>
2025-01-25 14:43:41 +0530

    Added types.h  which have macros for size and enum for return status

---
+++ types.h
@@ -0,0 +1,14 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+#define USER_NAME_SIZE 30
+#define USER_PASS_SIZE 20
+
+/* Status will be used in fn. return type */
+typedef enum
+{
+	v_success,
+	v_failure
+} Status;
+
+#endif
