e01c9f08d1241cb61ba3c26603750de601d4be7e
N.Yaswanth <yaswanth.n@priyarajaelectronics.com>
2025-01-25 13:01:01 +0530

    Modified- added one more structure for handling files

--- main_header.h
+++ main_header.h
@@ -5,17 +5,36 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+/* Status will be used in fn. return type */
 typedef enum
 {
 	v_success,
 	v_failure
-}Status;
+} Status;
 
+/* 
+ * Structure to store user information required for
+ * login to take test
+ */
 typedef struct USER
 {
 	char name[30];
-	char email[50];
 	char pass[30];
-}Usr_Info;
+} Usr_Info;
+
+/* 
+ * Structure to store FILES information required for
+ * opening and modifying the data
+ */
+typedef struct FILES
+{
+	FILE *fptr_Users_data_file;
+	FILE *fptr_Password_file;
+	FILE *fptr_Question_bank_file;
+	FILE *fptr_Answers_file;
+} File_Info;
+
+Status 
 Status signup(Usr_Info *User, FILE *Usr_file);
+
 #endif
