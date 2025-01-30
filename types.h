d5b47d7113719e3a525aba9bc9755615e8b5a4a6
N.Yaswanth <yaswanth.n@priyarajaelectronics.com>
2025-01-30 18:43:39 +0530

    Added some more macros

--- types.h
+++ types.h
@@ -2,7 +2,10 @@
 #define TYPES_H
 
 #define USER_NAME_SIZE 30
-#define USER_PASS_SIZE 20
+#define USER_PASS_SIZE 30
+
+#define MAX_ATTEMPTS 5
+#define TIME_FOR_EACH_QUE 10
 
 /* Status will be used in fn. return type */
 typedef enum
