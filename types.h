8dbe204b120dd100c4c45012379813dc2665c9d8
N.Yaswanth <yaswanth.n@priyarajaelectronics.com>
2025-02-11 17:58:29 +0530

    Added macros for Max questions, time for each question, max attempts to enter password

--- types.h
+++ types.h
@@ -4,7 +4,10 @@
 #define USER_NAME_SIZE 30
 #define USER_PASS_SIZE 30
 
-#define MAX_ATTEMPTS 5
+#define MAX_ATTEMPTS 3
+
+#define MAX_QUESTIONS 5
+#define MAX_ANSWER_LEN 50
 #define TIME_FOR_EACH_QUE 10
 
 /* Status will be used in fn. return type */
