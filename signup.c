9be6de9adb97504a665f900fa82e89603bdb1437
N.Yaswanth <yaswanth.n@priyarajaelectronics.com>
2025-02-11 17:50:36 +0530

    Modified Signup function which works fine now

--- signup.c
+++ signup.c
@@ -4,18 +4,19 @@ Status signup(Usr_Info *User, FILE *Usr_data_fname, FILE *Usr_pass_fname)
 {
 
 label:  printf("\nCreate Your User Name: ");
-        getchar();
-        scanf("%[^\n]",User->name);
+        scanf(" %[^\n]",User->name);
+        
+        clear_screen();
 
         if (user_exist(User->name, Usr_data_fname) == v_success)
         {   
-            printf("\nINFO: \033[031mUser Name Already Exist\033[0m\n");
-            printf("INFO: \033[031mTry Again\033[0m\n");
+            printf("\nINFO: \033[031m User Name Already Exists\033[0m\n");
+            printf("INFO: \033[031m Try Again\033[0m\n");    
             goto label;
         }
         else
         {
-            printf("\nINFO: \033[32mThis User Name is Exists\033[0m\n");
+            printf("\nINFO: \033[32m This User Name is Exists\033[0m\n");
 
             char re_enter[USER_PASS_SIZE];
             char pass_show_flag;
@@ -23,10 +24,10 @@ label:  printf("\nCreate Your User Name: ");
 
             while (failed_attempts) 
             {
-                printf("\nINFO: \033[33mPassword length must be atleast 8 character's\n");
-                printf("      Password must contain at least one uppercase letter, one digit, and one special character\n\033[0m\n");
+                printf("\nINFO: \033[33m Password length must be atleast 8 character's\n");
+                printf("       Password must contain at least one uppercase letter, one digit, and one special character\n\033[0m\n");
 
-                printf("\nINFO: \033[1mIf You Want to see the Password Press (D or d- Dispaly) or (H or h- Hide): \033[0m");
+                printf("\nINFO: \033[1m If You Want to see the Password Press (D or d- Dispaly) or (H or h- Hide): \033[0m");
                 scanf(" %c",&pass_show_flag);
                 
                 if(pass_show_flag == 'D' || pass_show_flag == 'd')
@@ -44,30 +45,33 @@ label:  printf("\nCreate Your User Name: ");
                 }
                 else
                 {
-                    printf("\nINFO: \033[31mInvaild Option, Enter (D or d- Dispaly) or (H or h- Hide)\033[0m\n");
+                    printf("\nINFO: \033[31m Invaild Option, Enter (D or d- Dispaly) or (H or h- Hide)\033[0m\n");
                     continue;
                 }
 
                 if (is_valid_password(User->password) == v_failure || strlen(User->password) < 8) 
                 {
-                    printf("\nINFO: \033[31mPassword does not meet the required criteria. Make sure to consider the below criteria\033[0m\n");
+                    printf("\nINFO: \033[31m Password does not meet the required criteria. Make sure to consider the below criteria\033[0m\n");
                     failed_attempts--;
                     continue;
                 }
 
                 if (strcmp(User->password, re_enter) == 0) 
                 {
+                    encrypt_password(User->password);
+                    printf("%s\n", User->password);
                     break;
                 } 
                 else 
                 {
-                    printf("\nINFO: \033[31mPassword Not Match\n\tTry Again\033[0m\n\n");
                     failed_attempts--;
+                    clear_screen();
+                    printf("\nINFO: \033[31m Incorrect password. Please try again. You Have %hd Attempts Left\033[0m\n", failed_attempts);
                 }
             }
             if (!failed_attempts)
             {
-                printf("\nINFO: \033[31mToo Many Failed Attempts, Exiting the process.\033[0m\n");
+                printf("\nINFO: \033[31m Too Many Failed Attempts, Exiting the process.\033[0m\n");
                 return v_failure;
             }
         }
@@ -81,7 +85,7 @@ label:  printf("\nCreate Your User Name: ");
         pos = ftell(Usr_pass_fname);
         fseek(Usr_pass_fname,pos-1,SEEK_CUR);
         fprintf(Usr_pass_fname, "%s,", User->password);
-
+        clear_screen();
         return v_success;
 }
 
@@ -94,7 +98,7 @@ Status user_exist(char *name, FILE *usr_file)
 
     while((ch = fgetc(usr_file)) != EOF)
     {
-        if (ch == ',' || ch == '\n')
+        if (ch == ',' || ch == '\n' || ch == ' ')
         {
             buf[i] = '\0';
             if (!strcmp(name, buf))
@@ -143,4 +147,31 @@ Status is_valid_password(char *password)
         return v_success;
     }
     return v_failure;
+}
+
+Status encrypt_password(char *user_password)
+{
+    char cipher_password[USER_PASS_SIZE];
+    strcpy(cipher_password, user_password);
+    for (int i = 0; cipher_password[i] != '\0'; i++)
+    {
+        if (isalpha(cipher_password[i]))
+        {
+            if (islower(cipher_password[i]))
+            {
+                user_password[i] = 'a' + ((cipher_password[i] - 'a' + cipher_password[i] - 'a') % 26);
+            }
+            else
+            {
+                user_password[i] = 'A' + ((cipher_password[i] - 'A' + cipher_password[i] - 'A') % 26);
+            }
+        }
+        else
+        {
+            user_password[i] = cipher_password[i];
+        }
+    }
+
+    user_password[strlen(cipher_password)] = '\0'; 
+    return v_success;
 }
